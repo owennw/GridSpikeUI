c1f7d8892afae9c5d4129eb6c6cb32dfa8d03c0a
Nick Owen <nowen@scottlogic.co.uk>
2017-05-09 15:39:13 +0100

    feat: debounce input from search box

--- src/customers/customer-search/customer-search.component.ts
+++ src/customers/customer-search/customer-search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Observable, Subject } from 'rxjs'
 
 import CustomerSearchService from './customer-search.service'
 import { ICustomer } from '../customer'
@@ -8,10 +9,20 @@ import { ICustomer } from '../customer'
   templateUrl: './customer-search.component.html',
 })
 
-export default class CustomerSearch {
+export default class CustomerSearch implements OnInit {
   customerSearchResults: ICustomer[] = []
+  searchQuery: string = ''
+  private keyUp = new Subject<string>()
 
   constructor(private customerSearchService: CustomerSearchService) { }
+
+  ngOnInit(): void {
+    this.keyUp
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .flatMap(search => Observable.of(search).delay(50))
+      .subscribe(data => this.searchQuery = data)
+  }
 
   search(query: any): Promise<ICustomer[]> {
     return this.customerSearchService.search(query)
